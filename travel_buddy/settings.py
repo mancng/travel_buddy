5ac77b57469117477bbb48b209c6f820e6d4c5bb
Ubuntu <ubuntu@ip-172-31-38-144.us-west-2.compute.internal>
2017-12-15 21:53:48 +0000

    first deployment

--- travel_buddy/settings.py
+++ travel_buddy/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6@d65q#%lgizu4fiei!uc$wq5a9m&4tm9te41owuu)2anb2(g&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.43.58.85']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
