4e399a3105a2c666ff1d4b566c64ca872cbb0685
rng <mancng@gmail.com>
2018-04-11 17:22:49 -0700

    changed back to local settings and updated .gitignore

--- travel_buddy/settings.py
+++ travel_buddy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '6@d65q#%lgizu4fiei!uc$wq5a9m&4tm9te41owuu)2anb2(g&'
 DEBUG = True
 
 # ALLOWED_HOSTS = ['52.43.58.85']
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['54.165.146.176']
 
 
 # Application definition
@@ -120,4 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+#  STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
